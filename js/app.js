540f94f9a8a7cef3ff19ceeb75a4da4e9f01b9f6
Jessica Grinberg <jessgrinberg@gmail.com>
2017-02-05 23:24:20 -0800

    added responsive menu

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@
 $(".navFlex a").on("click", function () {
     $(".navFlex a.active").removeClass("active");
     $(this).addClass("active");
+     $('#menu-toggle').prop('checked', false); 
+    // $(".navFlex #menu-toggle:checked ~ ul ").css({ opacity: "0" });
+     console.log('click')
 });
+
+// $('.navFlex a').on("click", function () {
+//   $('#menu-toggle').prop('checked', false); 
+//     console.log('click2')
+// })
